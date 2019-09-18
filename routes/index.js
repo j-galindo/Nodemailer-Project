a2047b1ed35aa4ea1020fe4c994346c5c69ac3fa
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-17 22:34:08 -0400

    working set email

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
-
+var nodemailer = require("nodemailer");
 /* GET home page */
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
 
+// let transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//         user: 'your email address',
+//         pass: 'your email password'
+//     }
+// });
+
 router.post('/send-email', (req, res, next) => {
     let { email, subject, message } = req.body;
-    res.render('message', { email, subject, message })
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'jgalindo.ironhack@gmail.com',
+            pass: 'Ms134life'
+        }
+    });
+    transporter.sendMail({
+            from: '"My Awesome Project 👻" <jgalindo.ironhack@gmail.com>',
+            to: email,
+            subject: subject,
+            text: message,
+            html: `<b>${message}</b>`
+        })
+        .then(info => res.render('message', { email, subject, message, info }))
+        .catch(error => console.log(error));
 });
 
 
